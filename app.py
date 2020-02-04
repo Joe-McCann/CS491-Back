a2e7a3d4c4dd374c230e58f099e9b42afb3cd279
Joe McCann <wjm9@njit.edu>
2020-02-04 18:07:12 -0500

    Next test

---
+++ app.py
@@ -0,0 +1,17 @@
+import falcon
+
+'''from .images import ImageResource
+from .authorize import Authorize
+from .patient import Patient
+from .doctor import Doctor'''
+
+api = application = falcon.API()
+
+'''images = ImageResource()
+auth = Authorize()
+pati = Patient()
+doc = Doctor()
+api.add_route('/images', images)
+api.add_route('/auth', auth)
+api.add_route('/patient', pati)
+api.add_route('/doc', doc)'''
