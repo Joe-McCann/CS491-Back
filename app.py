b4e1ff8e544b518800a19ed5524dd675be33fe96
Joe McCann <wjm9@njit.edu>
2020-02-11 20:14:56 -0500

    Events Stuff

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import falcon
 
 from helloWorld import HelloWorld
+from events import Events
 
 api = application = falcon.API()
 
 hw = HelloWorld()
+ev = Events()
 api.add_route('/helloWorld', hw)
+api.add_route('/events', ev)
---
+++ events.py
@@ -0,0 +1,15 @@
+import json
+import pymongo
+import falcon
+
+class Events(object):
+
+    def __init__(self):
+        with open("mongoCredentials.txt", "r") as file:
+            creds = [x.strip() for x in file.readlines()]
+            self.client = pymongo.MongoClient("mongodb+srv://"+creds[0]+":"+creds[1]+"@cluster0-rv4qi.mongodb.net/test?retryWrites=true&w=majority")
+            self.db = self.client["LetsHang"]
+            self.events = self.db["Events"]
+
+    def on_post(self, req, resp):
+        pass
