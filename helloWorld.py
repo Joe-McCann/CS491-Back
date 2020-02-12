b45a438303906cdb490655e2e788fe35acf9d34d
jebrildguev <Jebbiie@jebrils-mbp.resource.campus.njit.edu>
2020-02-11 20:10:42 -0500

    On post method added in Helloworld.py

--- helloWorld.py
+++ helloWorld.py
@@ -9,3 +9,11 @@ class HelloWorld(object):
         resp.body = json.dumps(ret_data, ensure_ascii=False)
         resp.status = falcon.HTTP_200
         return 
+
+    def on_post(self, req, resp):
+        data = json.loads(req.stream.read().decode('utf-8'))
+        respName = {"name": data["name"]}
+        resp.body = json.dumps(respName, ensure_ascii=False)
+        resp.status = falcon.HTTP_200
+        return 
+        
